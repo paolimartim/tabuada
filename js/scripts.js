2cfd2051e4d5e9d26b8123217cb2ff37dcc846a2
paolimartim <paolaoliveiramartimbianco@gmail.com>
2022-03-28 15:08:26 -0300

    projeto finalizado

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function multiplicar() {
+    var tabuada = document.getElementById("tabuada").value
+    var vezes = document.getElementById("vezes").value
+    var resultado = document.getElementById("resultado")
+    resultado.innerHTML = tabuada * vezes
+    
+}
